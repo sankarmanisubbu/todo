719dbebfc26cf1e4f9c4e1489a742f68b18937c5
sankarmanisubbu <sankarmanisubbu@gmail.com>
2024-02-19 18:51:44 +0530

    change to calc

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,12 @@
 import React from 'react';
 import './App.css';
 import Todos from './todo';
+import Calculator from './calculator';
 
 function App() {
   return (
-    <Todos />
+    //<Todos />
+    <Calculator />
   );
 }
 
